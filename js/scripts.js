4bfab0a131c4cdef660a70298c8864c470458fff
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-10-27 14:09:36 -0700

    got rid of type coercion

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@
     var noises = ["Boop!","Beep!","I'm sorry, Dave. I can't do that."];
     var numMatch = ["0","1","x"];
     var numberArray = [];
-    if (number%3 == 0){
+    if (number%3 === 0){
       return "I'm sorry Dave";
     }
     for (i = 0; i < number; i++) {
       numberArray.push(i);
-      // numberArray.toString();
+      numberArray.toString();
       // alert(numberArray);
     }//for loop1 end
       for (a = 0; a < numberArray.length; a++)
-        if (numberArray[a] == 0){
+        if (numberArray[a] === 0){
           var numberArray = numberArray.toString();
           var numberArray = numberArray.split("0").join("Boop!");
-        } else if (numberArray[a] == 1){
+        } else if (numberArray[a] === 1){
           var numberArray = numberArray.toString();
           var numberArray = numberArray.split("1").join("Beep!")
         }
