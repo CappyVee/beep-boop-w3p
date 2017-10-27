7575c971a7ff7eab5111f1ea5be460a6ee372efe
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-10-27 13:03:05 -0700

    can count number, zeros replaced by boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
   var beepMachine = function(number) {
+    var noises = ["Boop!","Beep!","I'm sorry, Dave. I can't do that."];
+    var numMatch = ["0","1","x"];
     var numberArray = [];
     for (i = 0; i < number; i++) {
       numberArray.push(i);
-      alert(numberArray);
-    }//for loop end
-    return numberArray.toString();
+      numberArray.toString();
+      // alert(numberArray);
+    }//for loop1 end
+      for (a = 0; a < numberArray.length; a++)
+        if (numberArray[a] ==0 0){
+          var numberArray = numberArray.toString();
+          var numberArray = numberArray.split("0").join("Boop!");
+        // } else if (numberArray[a] ==)
+
+        return numberArray;
+    // for (b = 0; b < numberArray.length; b++){
+    //   if
+    // }
   }//beepMachine end
 
 
