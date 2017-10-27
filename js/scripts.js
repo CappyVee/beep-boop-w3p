858e56d3547c90c27751cbb1ad284fb41bb781f4
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-10-27 11:45:01 -0700

    still trying

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@
     var numberArray = [];
     for (i = 0; i < number; i++) {
       numberArray.push(i);
-      // alert(numberArray);
+      alert(numberArray);
     }//for loop end
-    return numberArray.join("");
-    numberArray.toString;
-    $(".result").text(numberArray);
+    return numberArray.toString();
+    $(".result").text(result);
   }//beepMachine end
 
 
