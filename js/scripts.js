bcc992d61b4e9251025d82497953a4c7876226b5
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-11-01 20:34:11 -0700

    reworking specs, rebuilding js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
-  var beepMachine = function(number) {
-    // var noises = ["Boop!","Beep!","I'm sorry, Dave. I can't do that."];
-    // var numMatch = ["0","1","x"];
-    var numberArray = [];
-    if (number%3 === 0){
-      return "I'm sorry, Dave. I can't do that";
-    }
-    for (i = 0; i < number; i++) {
-      numberArray.push(i);
-      // numberArray.toString();
-      // alert(numberArray);
-    }//for loop1 end
-      for (a = 0; a < numberArray.length; a++)
-        if (numberArray[a] === 0){
-          var numberArray = numberArray.toString();
-          var numberArray = numberArray.split("0").join("Boop ");
-          // var numberArray = parseInt(numberArray);
-        } else if (numberArray[a] === "1"){
-          var numberArray = numberArray.toString();
-          var numberArray = numberArray.split("1").join("Beep ")
-        }
-
-        return numberArray;
-    // for (b = 0; b < numberArray.length; b++){
-    //   if
-    // }
-  }//beepMachine end
-
+  // var beepMachine = function(number) {
+  //
+  //   var numberArray = [];
+  //   if (number%3 === 0){
+  //     return "I'm sorry, Dave. I can't do that";
+  //   }
+  //   for (i = 0; i < number; i++) {
+  //     numberArray.push(i);
+  //   }//for loop1 end
+  //     for (a = 0; a < numberArray.length; a++)
+  //       if (numberArray[a] === 0){
+  //         var numberArray = numberArray.toString();
+  //         var numberArray = numberArray.split("0").join("Boop ");
+  //         // var numberArray = parseInt(numberArray);
+  //       } else if (numberArray[a] === "1"){
+  //         var numberArray = numberArray.toString();
+  //         var numberArray = numberArray.split("1").join("Beep ")
+  //       }
+  //
+  //       return numberArray;
+  // }//beepMachine end
+var beepMachine = function(number) {
+  if (number === 0) {
+    return ["Boop!"];
+  } else if (number === 1) {
+    return ["Boop!","Beep!"];
+  }
+}//end beepmachine fxn
 
 
 
